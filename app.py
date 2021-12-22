2d8d49e6f649e0bebdc9e08eee328c99523c2a87
ChronoLogic <msuttie12@hotmail.com>
2021-12-22 11:57:49 +0000

    Add app.run

--- app.py
+++ app.py
@@ -157,3 +157,9 @@ def logout():
     flash("You have been logged out")
     session.pop("user")
     return redirect(url_for("login"))
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=False)
